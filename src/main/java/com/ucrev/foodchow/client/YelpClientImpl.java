1a94df4b677ceab3f1ba35a9d92286aa4dd9868c
Baargav Venkatesh <kon7341@ex.kroger.com>
2014-04-05 19:27:08 -0400

    Trying to Create a Restclient

--- src/main/java/com/ucrev/foodchow/client/YelpClientImpl.java
+++ src/main/java/com/ucrev/foodchow/client/YelpClientImpl.java
@@ -1,11 +1,15 @@
 package com.ucrev.foodchow.client;
 
+import org.springframework.web.client.RestTemplate;
+
 /**
  * Created by ECSB Dev on 4/5/14.
  */
 public class YelpClientImpl implements YelpClient {
+
+    RestTemplate restTemplate = new RestTemplate();
     @Override
     public String getRestaurents() {
-        return null;
+       return null;
     }
 }
