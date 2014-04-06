cfdf5aa8f82739b9735ac41383c16d39a3381ebf
Baargav Venkatesh <kon7341@ex.kroger.com>
2014-04-05 23:44:06 -0400

    sumar commit

--- src/main/java/com/ucrev/foodchow/dto/ImageVector.java
+++ src/main/java/com/ucrev/foodchow/dto/ImageVector.java
@@ -3,4 +3,20 @@ package com.ucrev.foodchow.dto;
 public class ImageVector {
     private String imageUrl;
     private int[] imageVectorArray;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int[] getImageVectorArray() {
+        return imageVectorArray;
+    }
+
+    public void setImageVectorArray(int[] imageVectorArray) {
+        this.imageVectorArray = imageVectorArray;
+    }
 }
