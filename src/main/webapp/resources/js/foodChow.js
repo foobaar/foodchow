5302117fde9df0323c364811d0ea314f3ec852a5
Baargav Venkatesh <kon7341@ex.kroger.com>
2014-05-06 01:59:32 -0400

    Removed slash food from urls

--- src/main/webapp/resources/js/foodChow.js
+++ src/main/webapp/resources/js/foodChow.js
@@ -21,7 +21,7 @@ function start(aTag){
 function sendZipcode(zip){
     // call service with zipcode - GET
     status = 0;
-    xmlhttp.open("GET","http://localhost:8080/food/initialize?zip=" + zip,true);
+    xmlhttp.open("GET","http://localhost:8080/initialize?zip=" + zip,true);
     xmlhttp.send();
     //resp = getZipResp(); // only for testing
     //alert(resp.guid);	 // only for testing
@@ -62,7 +62,7 @@ function addImage(imageIndex,response){
         status = 1;
         //document.getElementById('zipcode').value = (JSON.stringify(searchQuery));
         searchQuery.zipcode = document.getElementById('zipcode').value;
-        xmlhttp.open("POST","http://localhost:8080/food/recommmend",true);
+        xmlhttp.open("POST","http://localhost:8080/recommmend",true);
         xmlhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
         xmlhttp.send(JSON.stringify(searchQuery));
         //resp = getResultsResp(); // only for testing
